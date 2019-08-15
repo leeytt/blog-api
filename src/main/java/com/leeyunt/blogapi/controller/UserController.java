2b3b000308d1ae3ff3ec5130f1afc15f29252fa3
leeyunt <924721970@qq.com>
2019-08-15 17:30:33 +0800

    CURD管理员API🔨
    Signed-off-by: leeyunt <924721970@qq.com>

--- src/main/java/com/leeyunt/blogapi/controller/UserController.java
+++ src/main/java/com/leeyunt/blogapi/controller/UserController.java
@@ -4,11 +4,11 @@ package com.leeyunt.blogapi.controller;
 import com.leeyunt.blogapi.entity.User;
 import com.leeyunt.blogapi.service.UserService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,20 +22,66 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/user")
-@Api(value = "管理员",tags="管理员")
+@Api(tags="管理员相关Api")
 public class UserController {
     @Autowired
     private UserService userService;
 
     /**
      * 获取管理员列表
-     *
-     * @author leeyunt
-     * @since 2019-08-15
+     * @return List<User>
      */
-    @RequestMapping(value="/getAll",method = RequestMethod.GET)
-    @ApiOperation(value = "获取管理员列表",notes = "获取管理员列表")
+    @GetMapping("/getAll")
+    @ApiOperation(value = "获取列表", notes = "获取所有记录")
     public List<User> getAll(){
         return userService.list();
+    }
+
+    /**
+     * 添加管理员
+     * @return Boolean
+     */
+    @PostMapping("/addUser")
+    @ApiOperation(value = "添加", notes = "添加记录")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "username", value = "账号", defaultValue = "李四", required = true),
+            @ApiImplicitParam(name = "password", value = "密码", defaultValue = "123456", required = true)
+    })
+    public Boolean addUser(@RequestParam("username") String username, @RequestParam("password") String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        return userService.save(user);
+    }
+
+    /**
+     * 根据id查询管理员
+     * @return User
+     */
+    @GetMapping("/{userId}")
+    @ApiOperation(value = "根据id查询", notes = "根据id查询记录")
+    @ApiImplicitParam(name = "userId", value = "用户id", defaultValue = "1", required = true)
+    public User getUserById(@PathVariable("userId") Integer userId) {
+        return userService.getById(userId);
+    }
+
+    /**
+     * 根据id动态更新管理员
+     */
+    @PutMapping("/updateUser")
+    @ApiOperation(value = "根据id动态更新", notes = "根据id动态更新记录")
+    public Boolean updateUserById(@RequestBody User user) {
+        return userService.saveOrUpdate(user);
+    }
+
+    /**
+     * 根据id删除管理员
+     * @return boolean
+     */
+    @DeleteMapping("/{userId}")
+    @ApiOperation(value = "根据id删除", notes = "根据id删除记录")
+    @ApiImplicitParam(name = "userId", value = "用户id", defaultValue = "99", required = true)
+    public boolean deleteUserById(@PathVariable("userId") Integer userId) {
+        return userService.removeById(userId);
     }
 }
