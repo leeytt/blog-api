fd33e095a054295378526fa1257bc8e35aff4dac
leeyunt <924721970@qq.com>
2019-08-18 08:50:44 +0800

    统一定义返回类🔨
    Signed-off-by: leeyunt <924721970@qq.com>

--- src/main/java/com/leeyunt/blogapi/utils/ResultUtil.java
+++ src/main/java/com/leeyunt/blogapi/utils/ResultUtil.java
@@ -1,11 +1,25 @@
 package com.leeyunt.blogapi.utils;
 
 /**
- * 统一定义返回类
+ * 统一定义返回结果类
  *
  * @author leeyunt
  * @since 2019/8/16
  */
 public class ResultUtil {
+    /**
+     * 返回状态码
+     */
+    private Integer code;//状态码，成功；1、失败；0
+
+    /**
+     * 返回消息
+     */
+    private String msg;//消息
+
+    /**
+     * 返回数据
+     */
+    private Object data;//数据
 
 }
